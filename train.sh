e81a50c4086a2b0e8b1621744cdeaa8d49de7e6f
kkkamur07 <Krrishmof07@gmail.com>
2026-05-21 14:47:48 +0530

    added final changes.

--- train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while [ $(nvidia-smi --query-compute-apps=pid --format=csv,noheader | wc -l) -gt 0 ]; do 
-    echo "GPU busy, waiting... $(date)"
-    sleep 60
-done
-echo "GPU free! Starting job at $(date)"
-python3 -m ocr.main
